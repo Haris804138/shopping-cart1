b961ecbdaa7103997444a4e7316e48ad34a90152
Haris Baloch <harib7590@gmail.com>
2026-04-29 12:28:21 +0500

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import { CartProvider } from "../context/CartContext";
+import Navbar from "../components/Navbar";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <CartProvider>
+          <Navbar />
+          {children}
+        </CartProvider>
+      </body>
+    </html>
+  );
+}
